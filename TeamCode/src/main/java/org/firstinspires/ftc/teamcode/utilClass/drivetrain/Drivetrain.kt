b9bc0ef6b27f15c598053a2c7b8b0fae182b8bdd
Graden <101433010+GramGra07@users.noreply.github.com>
2024-09-23 09:23:30 -0600

    update dt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilClass/drivetrain/Drivetrain.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilClass/drivetrain/Drivetrain.kt
@@ -39,7 +39,7 @@ class Drivetrain(
         var drivetrains: MutableList<Drivetrain> = mutableListOf(
             Drivetrain(
                 DrivetrainNames.MAIN,
-                DrivetrainType.TANK,
+                DrivetrainType.MECANUM,
                 DrivePARAMS(
                     783.0500500149055,
                     1678.229840435962,
