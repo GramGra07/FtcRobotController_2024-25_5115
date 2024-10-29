42132855f5ff209cfd4b4bb5093077e22e03f3c9
Graden <101433010+GramGra07@users.noreply.github.com>
2024-10-28 19:15:26 -0600

    added reset to dual encoder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/customHardware/sensors/DualEncoder.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/customHardware/sensors/DualEncoder.kt
@@ -1,23 +1,36 @@
 package org.firstinspires.ftc.teamcode.customHardware.sensors
 
+import com.qualcomm.robotcore.hardware.DcMotor
 import com.qualcomm.robotcore.hardware.DcMotorEx
 import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.robotcore.external.Telemetry
-import org.firstinspires.ftc.teamcode.followers.pedroPathing.localization.Encoder
 
-class DualEncoder(hw: HardwareMap, val name1: String, val name2: String, val subsystemName: String) {
+class DualEncoder(
+    hw: HardwareMap,
+    private val name1: String,
+    private val name2: String,
+    private val subsystemName: String
+) {
     private val encoder: DcMotorEx
     private val reversedEncoder: DcMotorEx
+
     init {
         encoder = hw.get(DcMotorEx::class.java, name1)
         reversedEncoder = hw.get(DcMotorEx::class.java, name2)
+        resetEncoders()
     }
+
     fun getAverage(): Double {
         return (encoder.getPosition() + (reversedEncoder.getPosition(true))) / 2.0
     }
+
     fun telemetry(telemetry: Telemetry) {
         telemetry.addData("$subsystemName Encoder", "%.1f", encoder.getPosition())
-        telemetry.addData("$subsystemName Reversed encoder", "%.1f", reversedEncoder.getPosition(true))
+        telemetry.addData(
+            "$subsystemName Reversed encoder",
+            "%.1f",
+            reversedEncoder.getPosition(true)
+        )
         telemetry.addData("$subsystemName Average", "%.1f", getAverage())
     }
 
@@ -27,5 +40,12 @@ class DualEncoder(hw: HardwareMap, val name1: String, val name2: String, val sub
         } else {
             -this.currentPosition.toDouble()
         }
+    }
+
+    private fun resetEncoders() {
+        encoder.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        reversedEncoder.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
+        encoder.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        reversedEncoder.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
     }
 }
