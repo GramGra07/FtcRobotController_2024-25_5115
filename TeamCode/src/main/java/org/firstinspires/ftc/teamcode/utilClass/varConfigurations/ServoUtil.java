09ec62d849258f2fd4ad9406de30a4315e830f2f
Graden <101433010+GramGra07@users.noreply.github.com>
2024-11-12 20:26:51 -0700

    changed claw angle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilClass/varConfigurations/ServoUtil.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilClass/varConfigurations/ServoUtil.java
@@ -7,7 +7,7 @@ import com.acmerobotics.dashboard.config.Config;
 public class ServoUtil {
     public static double openClaw = 120;
 
-    public static double closeClaw = 60;
+    public static double closeClaw = 50;
 
     public static double rotateLeft = 0;
     public static double rotateCenter = 90;
