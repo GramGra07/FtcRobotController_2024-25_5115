d964f77f3f3f52ad421dee6bdce6e18eb1700fa4
Graden <101433010+GramGra07@users.noreply.github.com>
2024-12-12 14:39:00 -0700

    added pid tuner

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opModes/tuners/pidTuner.kt
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.opModes.tuners
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
+import org.firstinspires.ftc.teamcode.customHardware.HardwareConfig
+import org.firstinspires.ftc.teamcode.customHardware.autoUtil.StartLocation
+import org.firstinspires.ftc.teamcode.customHardware.autoUtil.startEnums.Alliance
+import org.firstinspires.ftc.teamcode.utilClass.GroupingTitles
+
+@TeleOp(group = GroupingTitles.TESTING) //@Disabled//disabling the opmode
+
+class pidTuner : LinearOpMode() {
+    //declaring the class
+    override fun runOpMode() { //if opmode is started
+        val robot = HardwareConfig(this, false, StartLocation(Alliance.RED))
+        waitForStart()
+        robot.once() //runs once
+        while (opModeIsActive()) { //while the op mode is active
+            robot.armSubsystem.setPitchTarget(400.0)
+            robot.armSubsystem.update()
+        }
+    }
+}
