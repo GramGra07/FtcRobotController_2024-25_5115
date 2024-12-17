df8303d9993860f4e48e551f7fe73fa00467523b
Graden <101433010+GramGra07@users.noreply.github.com>
2024-12-17 12:37:28 -0700

    added rr

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/followers/roadRunner/tuning/SplineTest.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.followers.roadRunner.tuning;
+
+import static org.firstinspires.ftc.teamcode.followers.roadRunner.tuning.TuningOpModes.DRIVE_CLASS;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.followers.roadRunner.MecanumDrive;
+import org.firstinspires.ftc.teamcode.followers.roadRunner.TankDrive;
+
+public final class SplineTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Pose2d beginPose = new Pose2d(0, 0, 0);
+        if (DRIVE_CLASS.equals(MecanumDrive.class)) {
+            MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+
+            waitForStart();
+
+            Actions.runBlocking(
+                    drive.actionBuilder(beginPose)
+                            .splineTo(new Vector2d(30, 30), Math.PI / 2)
+                            .splineTo(new Vector2d(0, 60), Math.PI)
+                            .build());
+        } else if (DRIVE_CLASS.equals(TankDrive.class)) {
+            TankDrive drive = new TankDrive(hardwareMap, beginPose);
+
+            waitForStart();
+
+            Actions.runBlocking(
+                    drive.actionBuilder(beginPose)
+                            .splineTo(new Vector2d(30, 30), Math.PI / 2)
+                            .splineTo(new Vector2d(0, 60), Math.PI)
+                            .build());
+        } else {
+            throw new RuntimeException();
+        }
+    }
+}
