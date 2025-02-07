17a64908d0a45e808449dc7cedded175ff47da46
Graden <101433010+GramGra07@users.noreply.github.com>
2025-02-07 09:18:14 -0700

    tune sample auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilClass/varConfigurations/AutoVars.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utilClass/varConfigurations/AutoVars.java
@@ -7,8 +7,8 @@ public class AutoVars {
     public static Double sampleLeftTurnOffset = -8.0;
     public static Double sampleCenterTurnOffset = -10.0;
     public static Double sampleRightTurnOffset =-17.0;
-    public static Double sampleLeftPositionOffset = -6.0;
+    public static Double sampleLeftPositionOffset = -5.0;
     public static Double sampleCenterPositionOffset = -6.0;
-    public static Double sampleRightPositionOffset = -7.0;
+    public static Double sampleRightPositionOffset = -6.0;
     public static Double scoringOffsetTurn = 0.0;
 }
